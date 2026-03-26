f3a211fc8b8f6576ab6a73402467975565e1312a
Julio Exzacarias <L22210299@tectijuana.edu.mx>
2026-03-25 21:18:52 -0700

    Add files via upload

---
+++ FabricaRestaurante/proyecto/Estilosservicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricasRestaurantes
+{
+    class ServicioMesa : IEstiloservicio
+    {
+        public string PrepararMesa() => "Mesa preparada con mantel y cubiertos";
+        public string ServirPlatillo() => "Platillo servido en mesa por el mesero";
+        public string CobrarCuenta() => "Cuenta presentada en mesa";
+    }
+}
+
+namespace FabricasRestaurantes
+{
+    class ServicioParaLlevar : IEstiloservicio
+    {
+        public string PrepararMesa() => "Empaque preparado para llevar";
+        public string ServirPlatillo() => "Platillo empacado en contenedor";
+        public string CobrarCuenta() => "Pago en caja antes de retirar";
+    }
+}
+
+namespace FabricasRestaurantes
+{
+    class ServicioExpress : IEstiloservicio
+    {
+        public string PrepararMesa() => "Pedido registrado para entrega express";
+        public string ServirPlatillo() => "Platillo entregado en puerta";
+        public string CobrarCuenta() => "Pago procesado en línea";
+    }
+}
+
+
+
---
+++ FabricaRestaurante/proyecto/PlatilloDecorador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricasRestaurantes
+{
+    public abstract class PlatilloDecorador : PlatoFuerte
+    {
+        protected PlatoFuerte _platillo;
+
+        public PlatilloDecorador(PlatoFuerte platillo)
+        {
+            _platillo = platillo;
+        }
+
+        public override string Servir() => _platillo.Servir();
+        public override double Costo => _platillo.Costo;
+    }
+}
